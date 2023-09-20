32fe81bef745d238b70dd11a8f698090c623b2db
Roshan Samuel <findrjsam@gmail.com>
2023-09-20 15:23:12 +0200

    Further improvements to the interface

--- nekReader.py
+++ nekReader.py
@@ -6,7 +6,6 @@ import itertools
 import numpy as np
 import xarray as xr
 from functools import partial
-import matplotlib.pyplot as plt
 from xarray.core.utils import Frozen
 
 # repeat() can be used as a stand-in for range()
@@ -28,14 +27,15 @@ class Elem:
 
 # ==============================================================================
 class HexaData:
-    def __init__(self, nel, lr1, dtype="float64"):
+    def __init__(self, nel, lr1):
         self.nel = nel
         self.lr1 = lr1
 
-        self.elem = [Elem(lr1, dtype) for _ in repeat(nel)]
+        self.elem = [Elem(lr1) for _ in repeat(nel)]
+
 
 # ==============================================================================
-class _NekDataStore(xr.backends.common.AbstractDataStore):
+class _xNekData(xr.backends.common.AbstractDataStore):
     axes = ("z", "y", "x")
 
     def __init__(self, elem):
@@ -65,41 +65,45 @@ class _NekDataStore(xr.backends.common.AbstractDataStore):
             "uy": xr.Variable(ax, elem.vel[1]),
             "uz": xr.Variable(ax, elem.vel[2]),
         }
-        if elem.pres.size:
-            data_vars["pressure"] = xr.Variable(ax, elem.pres[0])
 
-        if elem.temp.size:
-            data_vars["temperature"] = xr.Variable(ax, elem.temp[0])
+        data_vars["pressure"] = xr.Variable(ax, elem.pres[0])
+        data_vars["temperature"] = xr.Variable(ax, elem.temp[0])
 
         return Frozen(data_vars)
 
 
-def readnek(fname, dtype="float64", skip_vars=()):
-    try:
-        infile = open(fname, "rb")
-    except OSError as e:
-        print(f"I/O error ({e.errno}): {e.strerror}")
-        return -1
+if __name__ == "__main__":
+    print()
 
-    # ---------------------------------------------------------------------------
-    # READ HEADER
-    # ---------------------------------------------------------------------------
-    headData = infile.read(132).split()
+    try:
+        fName = sys.argv[1]
+        f = open(fName, 'rb')
+    except:
+        print("Could not read file :(\n")
+        exit()
+
+    headData = f.read(132).split()
+
+    try:
+        polyOrder = tuple([int(x) for x in headData[2:5]])
+    except:
+        print("Invalid file :(\n")
+        exit()
+
     precSize = int(headData[1])
-    polyOrder = tuple([int(x) for x in headData[2:5]])
     numElems = int(headData[5])
     solTime = float(headData[7])
     timeStep = int(headData[8])
     varList = str(headData[11])[2:-1]
-    ptsPerElem = np.prod(polyOrder)
 
-    if precSize == 4:
-        rType = "f"
-    elif precSize == 8:
-        rType = "d"
+    print("Polynomial order: ({0:2d}, {1:2d}, {2:2d})".format(*polyOrder))
+    print("Number of elements: {0:<15d}".format(numElems))
+    print("Solution time: {0:<11.5f}".format(solTime))
+    print("Time-step count: {0:<15d}".format(timeStep))
+    print("Variable list: {0:<5s}".format(varList))
 
-    # identify endian encoding
-    etagb = infile.read(4)
+    # Identifying endian encoding
+    etagb = f.read(4)
     etagL = struct.unpack("<f", etagb)[0]
     etagL = int(etagL * 1e5) / 1e5
     etagB = struct.unpack(">f", etagb)[0]
@@ -109,36 +113,47 @@ def readnek(fname, dtype="float64", skip_vars=()):
     elif etagB == 6.54321:
         emode = ">"
     else:
-        return -3
+        print("Invalid endian :(\n")
+        exit()
 
-    # read element map for the file
-    elmap = infile.read(4 * numElems)
-    elmap = struct.unpack(emode + numElems * "i", elmap)
+    # Set datatype of file
+    if precSize == 4:
+        rType = "f"
+    elif precSize == 8:
+        rType = "d"
+    else:
+        print("Invalid data precision :(\n")
+        exit()
 
-    # ---------------------------------------------------------------------------
-    # READ DATA
-    # ---------------------------------------------------------------------------
-    # initialize data structure
+    fDType = emode + rType
+
+    # Read element map for the file
+    elMap = f.read(4 * numElems)
+    elMap = struct.unpack(emode + numElems * "i", elMap)
+    elMap = np.array(elMap, dtype=np.int32)
+
+    ptsPerElem = np.prod(polyOrder)
+    bytesPerElem = ptsPerElem * precSize
+
+    # Create the HexaData class
     data = HexaData(numElems, polyOrder)
 
-    bytes_elem = ptsPerElem * precSize
 
-    def read_file_into_data(data_var, index_var=0):
-        """Read binary file into an array attribute of ``data.elem``"""
-        fi = infile.read(bytes_elem)
-        fi = np.frombuffer(fi, dtype=emode + rType, count=ptsPerElem)
+    def readData(dataArray):
+        fi = f.read(bytesPerElem)
+        fi = np.frombuffer(fi, dtype=fDType, count=ptsPerElem)
 
-        # Replace elem array in-place with
-        # array read from file after reshaping as
-        elem_shape = polyOrder[::-1]  # lz, ly, lx
-        data_var[index_var, ...] = fi.reshape(elem_shape)
+        elemShape = polyOrder[::-1]
+        dataArray = fi.reshape(elemShape)
 
-    # read geometry
+
     if varList[0] == 'X':
-        for iel in elmap:
-            el = data.elem[iel - 1]
-            for idim in range(3):
-                read_file_into_data(el.pos, idim)
+        # Read X, Y, Z coordinates
+        for elemInd in elMap:
+            inpElem = data.elem[elemInd - 1]
+            for iDim in range(3):
+                #readData(f, inpElem.pos[2-iDim], polyOrder, bytesPerElem, ptsPerElem, fDType)
+                readData(inpElem.pos[iDim])
     else:
         # Read grid data from file
         try:
@@ -154,91 +169,47 @@ def readnek(fname, dtype="float64", skip_vars=()):
         gelMap = struct.unpack(emode + numElems * "i", gelMap)
         gelMap = np.array(gelMap, dtype=np.int32)
 
-        if gelMap[0] != elmap[0]:
+        if gelMap[0] != elMap[0]:
             print("Grid file is incompatible :(\n")
             exit()
 
-        for iel in elmap:
-            el = data.elem[iel - 1]
-            for idim in range(3):
-                fi = g.read(bytes_elem)
-                fi = np.frombuffer(fi, dtype=emode + rType, count=ptsPerElem)
-                elem_shape = polyOrder[::-1]  # lz, ly, lx
-                el.pos[idim, ...] = fi.reshape(elem_shape)
+        for elemInd in elMap:
+            inpElem = data.elem[elemInd - 1]
+            for iDim in range(3):
+                #readData(g, inpElem.pos[iDim], polyOrder, bytesPerElem, ptsPerElem, fDType)
+                readData(inpElem.pos[iDim])
 
         g.close()
 
-    # read velocity
-    for iel in elmap:
-        el = data.elem[iel - 1]
-        for idim in range(3):
-            read_file_into_data(el.vel, idim)
+    # Read U, V, W velocities
+    for elemInd in elMap:
+        inpElem = data.elem[elemInd - 1]
+        for iDim in range(3):
+            #readData(f, inpElem.vel[iDim], polyOrder, bytesPerElem, ptsPerElem, fDType)
+            readData(inpElem.vel[iDim])
 
-    # read pressure
-    for iel in elmap:
-        el = data.elem[iel - 1]
-        read_file_into_data(el.pres)
+    # Read pressure
+    for elemInd in elMap:
+        inpElem = data.elem[elemInd - 1]
+        #readData(f, inpElem.pres, polyOrder, bytesPerElem, ptsPerElem, fDType)
+        readData(inpElem.pres)
 
-    # read temperature
-    for iel in elmap:
-        el = data.elem[iel - 1]
-        read_file_into_data(el.temp)
+    # Read temperature
+    for elemInd in elMap:
+        inpElem = data.elem[elemInd - 1]
+        #readData(f, inpElem.temp, polyOrder, bytesPerElem, ptsPerElem, fDType)
+        readData(inpElem.temp)
 
-    # close file
-    infile.close()
+    f.close()
 
+    print(data.elem[0].pos[0])
+    exit()
+
+    # Convert to xarray
     elements = data.elem
-    elem_stores = [_NekDataStore(elem) for elem in elements]
-    elem_dsets = [xr.Dataset.load_store(store).set_coords(store.axes) for store in elem_stores]
+    elemData = [_xNekData(elem) for elem in elements]
+    elemDSet = [xr.Dataset.load_store(store).set_coords(store.axes) for store in elemData]
 
-    ds = xr.combine_by_coords(elem_dsets, combine_attrs="drop")
+    #ds = xr.combine_by_coords(elemDSet, combine_attrs="drop")
 
-    # output
-    return ds
-
-
-if __name__ == "__main__":
-    try:
-        fName = sys.argv[1]
-    except:
-        print("Could not read file :(\n")
-        exit()
-
-    ds = readnek(fName)
-
-    x = np.array(ds['x'])
-    y = np.array(ds['y'])
-    z = np.array(ds['z'])
-
-    #np.savetxt("grid_x.dat", x)
-    #np.savetxt("grid_y.dat", y)
-    #np.savetxt("grid_z.dat", z)
-
-    T = ds['temperature']
-    Nz, Ny, Nx = T.shape
-
-    outData = np.zeros((5, Ny, Nx))
-
-    dTdz = -(T[1,:,:] - T[0,:,:])/(z[1] - z[0])
-    outData[0, :, :] = ds['ux'][1, :, :]
-    outData[1, :, :] = ds['uy'][1, :, :]
-    outData[2, :, :] = ds['uz'][1, :, :]
-    outData[3, :, :] = ds['pressure'][0, :, :]
-    outData[4, :, :] = dTdz[:, :]
-    np.save(fName + ".b_plate", outData)
-
-    dTdz = -(T[Nz-1,:,:] - T[Nz-2,:,:])/(z[Nz-1] - z[Nz-2])
-    outData[0, :, :] = ds['ux'][Nz-2, :, :]
-    outData[1, :, :] = ds['uy'][Nz-2, :, :]
-    outData[2, :, :] = ds['uz'][Nz-2, :, :]
-    outData[3, :, :] = ds['pressure'][Nz-2, :, :]
-    outData[4, :, :] = dTdz[:, :]
-    np.save(fName + ".t_plate", outData)
-
-    #X = ds['xmesh']
-    #Y = ds['ymesh']
-    #plt.contourf(X[0, :, :], Y[0, :, :], dTdz)
-    #plt.savefig("plot.png")
-    #exit()
-    #plt.contourf(X[:, 64, :], Z[:, 64, :], T[:, 64, :])
-    #plt.show()
+    print()
