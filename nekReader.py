83594de2e6b9a11927079435c9c7db06407a6513
Roshan Samuel <findrjsam@gmail.com>
2023-09-20 18:14:28 +0200

    Successful transfer of data to 3D arrays. Added HDF5 writing

--- nekReader.py
+++ nekReader.py
@@ -1,5 +1,6 @@
 import sys
 import struct
+import h5py as hp
 import numpy as np
 
 nelx, nely, nelz = 100, 100, 64
@@ -86,17 +87,11 @@ def readnek(fname, dtype="float64"):
     # close file
     infile.close()
 
+    print("Transferring data to 3D arrays")
+
     Nx = nelx*(polyOrder[0] - 1) + 1
     Ny = nely*(polyOrder[1] - 1) + 1
     Nz = nelz*(polyOrder[2] - 1) + 1
-
-    #x = np.zeros((Nx, Ny, Nz), dtype="float64")
-    #for i in range(nelx):
-    #    for j in range(nely):
-    #        for k in range(nelz):
-    #            elNum = i*nelx + j*nely + k
-    #print(Nx, Ny, Nz)
-    #exit()
 
     # Swap x and z axes correctly
     fData = np.swapaxes(fData, 2, 4)
@@ -104,10 +99,18 @@ def readnek(fname, dtype="float64"):
     # Bring all data to final axis
     fData = np.swapaxes(np.swapaxes(np.swapaxes(fData, 1, 2), 2, 3), 3, 4)
 
+    xPos = np.zeros(Nx, dtype="float64")
+    yPos = np.zeros(Ny, dtype="float64")
+    zPos = np.zeros(Nz, dtype="float64")
+
     xVel = np.zeros((Nx, Ny, Nz), dtype="float64")
     yVel = np.zeros((Nx, Ny, Nz), dtype="float64")
     zVel = np.zeros((Nx, Ny, Nz), dtype="float64")
 
+    prsr = np.zeros((Nx, Ny, Nz), dtype="float64")
+    tmpr = np.zeros((Nx, Ny, Nz), dtype="float64")
+
+    writex, writey, writez = True, True, True
     for elz in range(nelz):
         strz = elz * (polyOrder[2] - 1)
         lenz = polyOrder[2] - 1
@@ -115,6 +118,7 @@ def readnek(fname, dtype="float64"):
             lenz = polyOrder[2]
         endz = strz + lenz
 
+        writez = True
         for ely in range(nely):
             stry = ely * (polyOrder[1] - 1)
             leny = polyOrder[1] - 1
@@ -122,6 +126,7 @@ def readnek(fname, dtype="float64"):
                 leny = polyOrder[1]
             endy = stry + leny
 
+            writey = True
             for elx in range(nelx):
                 elNum = nelx*nely*elz + nelx*ely + elx
 
@@ -131,39 +136,24 @@ def readnek(fname, dtype="float64"):
                     lenx = polyOrder[0]
                 endx = strx + lenx
 
+                if writex: xPos[strx:endx] = fData[elNum, :lenx, 0, 0, 0]
+                if writey: yPos[stry:endy] = fData[elNum, 0, :leny, 0, 1]
+                if writez: zPos[strz:endz] = fData[elNum, 0, 0, :lenz, 2]
+
                 xVel[strx:endx, stry:endy, strz:endz] = fData[elNum, :lenx, :leny, :lenz, 3]
                 yVel[strx:endx, stry:endy, strz:endz] = fData[elNum, :lenx, :leny, :lenz, 4]
                 zVel[strx:endx, stry:endy, strz:endz] = fData[elNum, :lenx, :leny, :lenz, 5]
 
-    print(xVel[-3:, -3:, -3:])
-    exit()
-    # Reshape to just the data values
-    fData = fData.reshape(numElems*np.prod(polyOrder), 8)
+                prsr[strx:endx, stry:endy, strz:endz] = fData[elNum, :lenx, :leny, :lenz, 6]
+                tmpr[strx:endx, stry:endy, strz:endz] = fData[elNum, :lenx, :leny, :lenz, 7]
 
-    print(fData.shape)
-    fData = np.unique(fData, axis=0)
-    print(fData[:3, :])
-    print(fData.shape)
-    exit(0)
+                writey, writez = False, False
 
-    x = fData[0, ...].flatten()
-    y = fData[1, ...].flatten()
-    z = fData[2, ...].flatten()
-    u = fData[3, ...].flatten()
-    v = fData[4, ...].flatten()
-    w = fData[5, ...].flatten()
-    p = fData[6, ...].flatten()
-    t = fData[7, ...].flatten()
-
-    print(u.shape)
-
-    #print(fData.shape)
-    #print(fData[-1, 3, -1, -1, :])
-    #print(fData[-100, 3, 0, -1, :])
-    exit()
+            writex = False
+        writey = False
 
     # output
-    return np.array(fData)
+    return xPos, yPos, zPos, xVel, yVel, zVel, prsr, tmpr
 
 
 if __name__ == "__main__":
@@ -174,16 +164,21 @@ if __name__ == "__main__":
         print("Could not read file :(\n")
         exit()
 
-    ds = readnek(fName)
-    x = ds[:,0,:,:,:]
-    y = ds[:,1,:,:,:]
-    z = ds[:,2,:,:,:]
+    x, y, z, u, v, w, p, t = readnek(fName)
 
-    x = np.unique(x)
-    y = np.unique(y)
-    z = np.unique(z)
+    h5fName = fName + ".h5"
+    print("Writing output file: ", h5fName)
 
-    np.savetxt("grid_x.dat", x)
-    np.savetxt("grid_y.dat", y)
-    np.savetxt("grid_z.dat", z)
+    f = hp.File(h5fName, "w")
+
+    dset = f.create_dataset("X", data = x)
+    dset = f.create_dataset("Y", data = y)
+    dset = f.create_dataset("Z", data = z)
+    dset = f.create_dataset("U", data = u)
+    dset = f.create_dataset("V", data = v)
+    dset = f.create_dataset("W", data = w)
+    dset = f.create_dataset("P", data = p)
+    dset = f.create_dataset("T", data = t)
+
+    f.close()
 
